332aa1418e0a31db4986238a61c5cd95f985f949
Richard Gill <richard@zdev.io>
2017-05-24 17:17:36 +0100

    Adds messenger bot

---
+++ scripts/bot.js
@@ -0,0 +1,15 @@
+import http from 'http'
+import bot from './initialiseBot.js'
+
+bot.on('error', (error) => {
+  console.log(error.message)
+})
+
+bot.on('message', (payload, reply) => {
+  const message = payload.message.text
+  const senderId = payload.sender.id
+  bot.sendMessage(senderId, {text : message})
+})
+
+http.createServer(bot.middleware()).listen(3000)
+console.log('Echo bot server running at port 3000.')
