be2bfedc9c748ce60a5b058b354f0c8072d43146
Richard Gill <richard@zdev.io>
2017-05-24 13:44:26 +0100

    Adds a scheduled scrape of 80s every n minutes

---
+++ scripts/scraperWatch.js
@@ -0,0 +1,12 @@
+import later from 'later'
+import scraper from './scraper.js'
+
+const trackWatcher = (minutes, scrape, stationUrl, stationName, trackSelector, artistSelector) => {
+  const schedule = later.parse.recur().every(minutes).minute()
+  later.setInterval(
+    () => { scrape(stationUrl, stationName, trackSelector, artistSelector) },
+    schedule
+  )
+}
+
+trackWatcher(3, scraper, 'https://absoluteradio.co.uk/80s/music/', 'absolute80s', '.song-title a', '.song-artist')
