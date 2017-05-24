4ad51c4a2cc419d0d0f06d536bace4a24abf106a
Richard Gill <richard@zdev.io>
2017-05-24 17:36:30 +0100

    Adds postback listener for get started

--- scripts/bot.js
+++ scripts/bot.js
@@ -5,6 +5,24 @@ bot.on('error', (error) => {
   console.log(error.message)
 })
 
+bot.on('postback', (payload) => {
+  if (payload.postback.payload === 'GET_STARTED_PAYLOAD') {
+    const senderId = payload.sender.id
+
+    bot.getProfile(senderId, (error, profile) => {
+      if (error) {
+        console.log(error)
+      }
+
+      bot.sendMessage(
+        senderId,
+        {text : `Thanks ${profile.first_name},  you're now registered!`}
+      )
+      console.log(`${profile.first_name} ${profile.last_name} => users`)
+   })
+  }
+})
+
 bot.on('message', (payload) => {
   const message = payload.message.text
   const senderId = payload.sender.id
