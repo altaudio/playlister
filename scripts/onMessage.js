acc3e667d06f75f883a98bd65bbec2ab223c3a68
Richard Gill <richard@rgill.co.uk>
2017-06-14 15:03:20 +0100

    Uses spotify id from firebase

--- scripts/onMessage.js
+++ scripts/onMessage.js
@@ -2,12 +2,8 @@ import _ from 'lodash'
 import sendPlaylist from './sendPlaylist.js'
 
 export default (payload) => {
-  const message = payload.message.text
-  const splitMessage = _.split(message, ' ')
-
+  const requestedStation = payload.message.text
   const facebookId = payload.sender.id
-  const spotifyId = _.first(splitMessage)
-  const requestedStation = _.last(splitMessage)
 
-  sendPlaylist(facebookId, spotifyId, requestedStation)
+  sendPlaylist(facebookId, requestedStation)
 }
