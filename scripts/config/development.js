f8fe98459712e840591aa88a1394ebae95efba99
Richard Gill <richard@rgill.co.uk>
2017-06-15 11:25:54 +0100

    Adds station object file and maps over it in worker

--- scripts/config/development.js
+++ scripts/config/development.js
@@ -1,10 +1,10 @@
 export default {
   firebaseUrl: 'https://playlister-88b81.firebaseio.com/',
-  facebookAccessToken: 'EAACoDCOzpjYBAFchYD3vb3EfwBwYbsAMBmUXMGkRwHpkwagIXVLdswaQ4rVYE6FgIQ8YPk6K2GZAddIL6ZCOtNosPU4WCUyK66kJd4xukNAZCYgtbl52aWR2eDpBVyUjI6KQ2AOM7ajYZCtXBOU5ZBRm6uAvk6BPalVkFGeobZAwZDZD', // eslint-disable-line
+  facebookAccessToken: 'EAACoDCOzpjYBADYrBXpybMKQT9BtfYroDFNiZAkLOtTZBkKymua1InYtmhZCtEYynu4FZCvHZAe5YdovEFI03HtBDEA3yiPv80jNwrZCKkmnU38ggV3Oowpyy0AVXbzF4Ha3h6Mft6s5WMWKN5GJNaHYl6GZBhTyD1n7vUspDKyKgZDZD', // eslint-disable-line
   facebookAppSecret: '65052761b701874b2bfbc0558b889cdf',
   facebookVerifyToken: 'VERIFY_THIS_MELONFARMER',
   spotifyClientId: '20163bbbd7124ef6baaad77c60edb8ab',
   spotifyClientSecret: '83c87ed11ece4ce788bbe7505afb5cf4',
-  redirectUrl: 'https://2803b750.ngrok.io'
+  redirectUrl: 'https://4fcb3adf.ngrok.io'
 }
 
